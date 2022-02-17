c43797827e06be3942f3aeb8c5d0cd190f1f5896
Anna Everyday <amcabrera727@gmail.com>
2022-02-16 21:53:56 -0500

    git is easy

--- src/git/batch12.java
+++ src/git/batch12.java
@@ -5,6 +5,7 @@ public class batch12 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Today is Wednesday");
+System.out.println("Tomorrow is Thursday");
 	}
 
 }
